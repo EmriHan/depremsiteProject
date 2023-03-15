3eaffdf9c653899ebb04d7b52496344a6166f4ae
emirhann <86735658+EmriHan@users.noreply.github.com>
2023-03-15 03:37:00 +0300

    depremsiteProject 0.0.1
    Update 0.0.0

---
+++ codes/js/script.js
@@ -0,0 +1,28 @@
+const apiURL = "https://api.orhanaydogdu.com.tr/deprem/live.php";
+const tableBody = document.getElementById("deprem-tablosu");
+
+fetch(apiURL)
+  .then(response => response.json())
+  .then(data => {
+    const depremler = data.result;
+    for (let i = 0; i < 100; i++) {
+      const deprem = depremler[i];
+      const row = tableBody.insertRow();
+      row.insertCell().innerText = deprem.date;
+      const magCell = row.insertCell();
+      magCell.innerText = deprem.mag;
+      if (deprem.mag >= 4.5) {
+        magCell.classList.add("mag-red");
+      } else if (deprem.mag >= 3.5) {
+        magCell.classList.add("mag-yellow");
+      } else {
+        magCell.classList.add("mag-green");
+      }
+      row.insertCell().innerText = deprem.title;
+      row.insertCell().innerText = deprem.depth;
+    }
+  })
+  .catch(error => {
+    console.error("API err:", error);
+  });
+``
